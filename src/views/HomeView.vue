1c4e6623fd505bf7731feac4591b399880f87514
glebdyakov <glebdyakov2000@gmail.com>
2023-10-08 20:56:11 +0300

    add show more btn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,57 @@
 <template>
-  <v-row no-gutters>
-    <v-col
-      v-for="(item, i) in 4"
-      :key="i"
-      cols="12"
-      sm="6"
-      md="4"
-      lg="3"
-    >
-      <v-card
-        class="mx-auto my-12"
-        width="280"
+  <v-col cols="12" md="8" class="mx-auto">
+    <v-row no-gutters>
+      <v-col
+        v-for="(item, i) in 4"
+        :key="i"
+        cols="12"
+        sm="6"
+        md="6"
+        lg="5"
+        xl="3"
       >
-        <v-img
-          cover
-          height="250"
-          src="@/assets/item_preview.svg"
-        />
-        <v-card-item>
-          <v-card-title>Cashmere II</v-card-title>
+        <v-card
+          class="mx-auto my-12"
+          width="280"
+        >
+          <v-img
+            cover
+            height="250"
+            src="@/assets/item_preview.svg"
+          />
+          <v-card-item>
+            <v-card-title>Cashmere II</v-card-title>
 
-          <v-card-subtitle>
-            <span class="me-1">Покрывало</span>
-          </v-card-subtitle>
-        </v-card-item>
-        <v-card-actions>
-          <v-row
-            justify="space-between"
-            align="center"
-            class="mx-0"
-          >
-            <v-col>
-              <p>219 000 ₽</p>
-            </v-col>
-            <v-img 
-              contain
-              max-width="16.55"
-              max-height="20"
-              src="@/assets/buy.svg"
-            />
-          </v-row>  
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <v-card-subtitle>
+              <span class="me-1">Покрывало</span>
+            </v-card-subtitle>
+          </v-card-item>
+          <v-card-actions>
+            <v-row
+              justify="space-between"
+              align="center"
+              class="mx-0"
+            >
+              <v-col>
+                <p>219 000 ₽</p>
+              </v-col>
+              <v-img 
+                contain
+                max-width="16.55"
+                max-height="20"
+                src="@/assets/buy.svg"
+              />
+            </v-row>  
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="mt-5 mb-1">
+      <v-spacer />
+      <v-btn class="showMoreBtn" width="142" height="25">Показать еще</v-btn>
+    </v-row>
+    <v-footer height="150"></v-footer>
+  </v-col>
 </template>
 
 <script>
@@ -51,3 +59,9 @@ export default {
   name: 'HomeView',
 }
 </script>
+
+<style scoped>
+  .showMoreBtn {
+    text-transform: unset;
+  }
+</style>
