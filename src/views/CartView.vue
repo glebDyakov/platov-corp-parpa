f0a041867b6329d5275617af6bf7ee6ab12b177d
glebdyakov <glebdyakov2000@gmail.com>
2023-10-08 21:13:50 +0300

    add remove item handler

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -7,18 +7,19 @@
     >
       <v-card
         elevation="0"
-        width="500"
+        max-width="500"
         max-height="200"
         class="ma-0 pa-0"
       >
         <div class="ma-0 pa-0 d-flex flex-no-wrap justify-space-between">
           <v-img
             src="@/assets/item_preview.svg"
+            cover
           />
           <div class="flex-grow-1 pa-3">
             <v-row>
               <v-spacer></v-spacer>
-              <v-btn icon elevation="0">
+              <v-btn icon elevation="0" @click="removeItem(item)">
                 <v-icon>mdi-close</v-icon>
               </v-btn>
             </v-row>
@@ -38,6 +39,12 @@
 
 <script>
 export default {
-  name: 'CartView'
+  name: 'CartView',
+  methods: {
+    removeItem(item) {
+      // TODO
+      console.log(`remove item: ${item}`)
+    }
+  }
 }
 </script>
